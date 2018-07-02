3f40c9caba0a077a252e759db0e13809a3fa58a1
JoshuaBRussell <theruss007@gmail.com>
2018-07-02 00:46:24 -0500

    Update getEncoder_Rads_per_sec to correctly use delta_time as a parameter.

--- src/headers/encoders.h
+++ src/headers/encoders.h
@@ -4,6 +4,13 @@
 #include "stdint.h"
 
 /*----------------------------------------------------------------------------//
+//Defines                                                                     //
+/*----------------------------------------------------------------------------*/
+
+#define TICKS_PER_REV 64.0
+#define TICKS_TO_RADS_SF (6.283185/TICKS_PER_REV)
+
+/*----------------------------------------------------------------------------//
 // Functions for using a Quad Encoder with interrupts. Currently uses x4
 // Encoding.   
 //----------------------------------------------------------------------------*/
@@ -15,7 +22,7 @@ void setupEncoders(uint16_t port, uint16_t pinA, uint16_t pinB);
 //Returns the value of the current encoder count. 
 int getEncoderCount(void);
 
-float getEncoder_Rads_per_sec(float delta_t);
+float getEncoder_Rads_per_sec(float delta_t_sec);
 
 
 #endif // _ENCODERS_H
