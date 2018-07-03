6cd87effdaed03d7a05ef8aacf6ecbaf29f29651
JoshuaBRussell <theruss007@gmail.com>
2018-07-03 00:43:08 -0500

    Update Examples files to use main.h. Remove unneeded header files as well

--- Examples/PIDExample.c
+++ Examples/PIDExample.c
@@ -4,6 +4,7 @@
 //
 ////////////////////////////////////////////////////////////////////
 #include "xc.h"
+#include "main.h"
 #include "IO_Functions.h"
 #include "IO_Defines.h"
 #include "timer_functions.h"
@@ -12,37 +13,7 @@
 #include "pwm_module.h"
 #include "encoders.h"
 #include "motor_driver.h"
-
 #include <limits.h>
-
-#pragma config ICS = PGD1               // ICD Communication Channel Select bits (Communicate on PGEC1 and PGED1)
-#pragma config JTAGEN = OFF             // JTAG Enable bit (JTAG is disabled)
-
-// FPOR
-#pragma config ALTI2C1 = OFF            // Alternate I2C1 pins (I2C1 mapped to SDA1/SCL1 pins)
-#pragma config ALTI2C2 = OFF            // Alternate I2C2 pins (I2C2 mapped to SDA2/SCL2 pins)
-#pragma config WDTWIN = WIN25           // Watchdog Window Select bits (WDT Window is 25% of WDT period)
-
-// FWDT
-#pragma config WDTPOST = PS32768        // Watchdog Timer Postscaler bits (1:32,768)
-#pragma config WDTPRE = PR128           // Watchdog Timer Prescaler bit (1:128)
-#pragma config PLLKEN = OFF             // PLL Lock Enable bit (Clock switch will not wait for the PLL lock signal.)
-#pragma config WINDIS = OFF             // Watchdog Timer Window Enable bit (Watchdog Timer in Non-Window mode)
-#pragma config FWDTEN = OFF             // Watchdog Timer Enable bit (Watchdog timer enabled/disabled by user software)
-
-// FOSC
-#pragma config POSCMD = NONE            // Primary Oscillator Mode Select bits (Primary Oscillator disabled)
-#pragma config OSCIOFNC = OFF           // OSC2 Pin Function bit (OSC2 is clock output)
-#pragma config IOL1WAY = ON             // Peripheral pin select configuration (AlHIGH only one reconfiguration)
-#pragma config FCKSM = CSDCMD           // Clock Switching Mode bits (Both Clock switching and Fail-safe Clock Monitor are disabled)
-
-// FOSCSEL
-#pragma config FNOSC = FRC              // Oscillator Source Selection (Internal Fast RC (FRC))
-#pragma config IESO = OFF                // Two-speed Oscillator Start-up Enable bit (Start up device with FRC, then switch to user-selected oscillator source)
-
-// FGS
-#pragma config GWRP = OFF               // General Segment Write-Protect bit (General Segment may be written)
-#pragma config GCP = OFF                // General Segment Code-Protect bit (General Segment Code protect is Disabled)
 
 #define MOTOR_VOLTAGE 12.0
 #define DEBUG_PIN 15  //Port B Pin 15
