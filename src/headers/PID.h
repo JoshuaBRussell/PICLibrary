cce4c6a64aaccd245696ea603832f307fb68daeb
JoshuaBRussell <theruss007@gmail.com>
2018-07-04 18:44:19 -0500

    Update PID Module to have PID_ in front of function names.

--- src/headers/PID.h
+++ src/headers/PID.h
@@ -38,22 +38,22 @@ void PID_Init(PID* pid);
 
 //---- Getter Functions ----//
 //Gets the various PID coefficients
-float getKp(PID* pid);
-float getKi(PID* pid);
-float getKd(PID* pid);
+float PID_getKp(PID* pid);
+float PID_getKi(PID* pid);
+float PID_getKd(PID* pid);
 
 //Gets the currently established setpoint
-float getSetpoint(PID* pid);
+float PID_getSetpoint(PID* pid);
 
 //---- Setter Functions ----//
 //Sets the various values needed for the PID controller
-void setCoeff(PID* pid, float P, float I, float D);
-void setSetpoint(PID* pid, float setpoint);
-void setDeltaT(PID* pid, float deltaT);
-void setOutputLimits(PID* pid, float min, float max);
+void PID_setCoeff(PID* pid, float P, float I, float D);
+void PID_setSetpoint(PID* pid, float setpoint);
+void PID_setDeltaT(PID* pid, float deltaT);
+void PID_setOutputLimits(PID* pid, float min, float max);
 
 //Computes the output of the PID controller
-float Compute(PID* pid, float Input);
+float PID_Compute(PID* pid, float Input);
 #endif // _PID_H_
 
 
