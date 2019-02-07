247b4b30e36ba308a913fb7f5df1b932e1d462df
JoshuaBRussell <theruss007@gmail.com>
2019-02-07 17:26:12 -0600

    Add longer delay so ceedling doesn't screem before mdb is done.

--- test/simulation/sim_test_fixture_mdb.rb
+++ test/simulation/sim_test_fixture_mdb.rb
@@ -2,7 +2,7 @@ OUT_FILE = "test/simulation/out.txt"
 File.delete OUT_FILE if File.exists? OUT_FILE
 IO.popen("/usr/bin/mplab/bin/mdb.sh test/simulation/sim_instructions_mdb.txt")
 
-sleep 5
+sleep 10
 if File.exists? OUT_FILE
     file_contents = File.read OUT_FILE
     #file_contents.gsub!("\n", "")
