ccf2bf2e5f6b06cea4bea5d31edffa7a2023ca49
JoshuaBRussell <theruss007@gmail.com>
2019-02-16 08:42:15 -0600

    Fixed errors in test since using new simulation method.

--- test/test_io_functions.c
+++ test/test_io_functions.c
@@ -34,6 +34,8 @@
 #pragma config GCP = OFF                // General Segment Code-Protect bit (General Segment Code protect is Disabled)
 
 
+#define PORTA_PIN_MASK 0x001F //A0-A4
+#define PORTB_PIN_MASK 0xFFFF //B0-B15
 
 void setUp(void)
 {
@@ -52,25 +54,25 @@ void tearDown(void)
 void test_set_PinA0_as_Output(void)
 {
     setPinOut(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0xFFFE, TRISA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0xFFFE, TRISA);
 
 }
 
 void test_set_PinA1_as_Output(void){
     setPinOut(0, 1);
-    TEST_ASSERT_BITS(0xFFFF, 0xFFFD, TRISA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0xFFFD, TRISA);
 }
 
-void test_set_PinA15_as_Output(void){
-    setPinOut(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x7FFF, TRISA);
+void test_set_PinA4_as_Output(void){
+    setPinOut(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0xFFEF, TRISA);
 }
 
 void test_set_multiple_Pins_as_Output(void){
-    setPinOut(0, 15);
+    setPinOut(0, 4);
     setPinOut(0, 1);
     setPinOut(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0x7FFC, TRISA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0xFFEC, TRISA);
 }
 
 //----Test Port A TRIS as Input----//
@@ -78,28 +80,28 @@ void test_set_PinA0_as_Input(void)
 {
     TRISA = 0x0000;
     setPinIn(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0x0001, TRISA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0001, TRISA);
 
 }
 
 void test_set_PinA1_as_Input(void){
     TRISA = 0x0000;
     setPinIn(0, 1);
-    TEST_ASSERT_BITS(0xFFFF, 0x0002, TRISA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0002, TRISA);
 }
 
-void test_set_PinA15_as_Input(void){
+void test_set_PinA4_as_Input(void){
     TRISA = 0x0000;
-    setPinIn(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x8000, TRISA);
+    setPinIn(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0010, TRISA);
 }
 
 void test_set_multiple_Pins_as_Input(void){
     TRISA = 0x0000;
-    setPinIn(0, 15);
+    setPinIn(0, 4);
     setPinIn(0, 1);
     setPinIn(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0x8003, TRISA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0013, TRISA);
 }
 
 //----Test Port A Read----//
@@ -112,69 +114,69 @@ void test_set_multiple_Pins_as_Input(void){
 
 void test_set_PinA0_HIGH(void){
     setPinHigh(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0x0001, LATA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0001, LATA);
 }
 
 void test_set_PinA1_HIGH(void){
     setPinHigh(0, 1);
-    TEST_ASSERT_BITS(0xFFFF, 0x0002, LATA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0002, LATA);
 }
 
-void test_set_PinA15_HIGH(void){
-    setPinHigh(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x8000, LATA);
+void test_set_PinA4_HIGH(void){
+    setPinHigh(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0010, LATA);
 }
 
 void test_set_multiple_Pins_High(void){
     setPinHigh(0, 0);
     setPinHigh(0, 1);
-    setPinHigh(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x8003, LATA);
+    setPinHigh(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0013, LATA);
 }
 
 //---- Test Port A LAT Low ----//
 void test_set_PinA0_Low(void){
     LATA = 0xFFFF;
     setPinLow(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0xFFFE, LATA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0xFFFE, LATA);
 }
 
 void test_set_PinA1_Low(void){
     LATA = 0xFFFF;
     setPinLow(0, 1);
-    TEST_ASSERT_BITS(0xFFFF, 0xFFFD, LATA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0xFFFD, LATA);
 }
 
-void test_set_PinA15_Low(void){
+void test_set_PinA4_Low(void){
     LATA = 0xFFFF;
-    setPinLow(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x7FFF, LATA);
+    setPinLow(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0xFFEF, LATA);
 }
 
 void test_set_multiple_Pins_Low(void){
     LATA = 0xFFFF;
     setPinLow(0, 0);
     setPinLow(0, 1);
-    setPinLow(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x7FFC, LATA);
+    setPinLow(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0xFFEC, LATA);
 }
 
 //----Test Port A LAT Toggle ----//
 void test_toggle_PinA0_Low_to_High(void){
     togglePin(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0x0001, LATA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0001, LATA);
 }
 
 void test_toggle_PinA1_Low_to_High(void){
     togglePin(0, 1);
-    TEST_ASSERT_BITS(0xFFFF, 0x0002, LATA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0002, LATA);
 }
 
 void test_toggle_muli_pins_High(void){
     togglePin(0, 0);
     togglePin(0, 1);
-    togglePin(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x8003, LATA);
+    togglePin(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0013, LATA);
 }
 
 
@@ -182,15 +184,15 @@ void test_toggle_muli_pins_Low(void){
     LATA = 0xFFFF;
     togglePin(0, 0);
     togglePin(0, 1);
-    togglePin(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, ~(0x8003), LATA);
+    togglePin(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, ~(0x0013), LATA);
 }
 
 void test_toggle_muli_pins_mixed(void){
     togglePin(0, 0);
     togglePin(0, 1);
     togglePin(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0x0002, LATA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0002, LATA);
 }
 
 //----Test IO_Config ----//
@@ -234,8 +236,8 @@ void test_IO_Config_correctly_sets_all_pins(void){
 
     };
     IO_Config(IO_CONFIG_TABLE);
-    TEST_ASSERT_BITS(0xFFFF, 0xFFFF, LATA);
-    TEST_ASSERT_BITS(0xFFFF, 0x0000, TRISA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0xFFFF, LATA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0000, TRISA);
 }
 
 void test_IO_Config_correctly_sets_all_pins_alternating(void){
@@ -278,89 +280,89 @@ void test_IO_Config_correctly_sets_all_pins_alternating(void){
 
     };
     IO_Config(IO_CONFIG_TABLE);
-    TEST_ASSERT_BITS(0xFFFF, 0x5555, LATA);
-    TEST_ASSERT_BITS(0xFFFF, 0x5555, TRISA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x5555, LATA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x5555, TRISA);
 }
 
 //----Test Setting Open Drain----//
 void test_set_ODCA0(void){
     ODCA = 0x0000;
     setPinOD(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0x0001, ODCA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0001, ODCA);
 }
 
 void test_set_ODCA1(void){
     ODCA = 0x0000;
     setPinOD(0, 1);
-    TEST_ASSERT_BITS(0xFFFF, 0x0002, ODCA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0002, ODCA);
 }
 
-void test_set_ODCA15(void){
+void test_set_ODCA4(void){
     ODCA = 0x0000;
-    setPinOD(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x8000, ODCA);
+    setPinOD(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0010, ODCA);
 }
 
 void test_set_multiple_Pins_OD(void){
     ODCA = 0x0000;
     setPinOD(0, 0);
     setPinOD(0, 1);
-    setPinOD(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x8003, ODCA);
+    setPinOD(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0013, ODCA);
 }
 
 //----Test Setting Pull Up----//
 void test_set_CNPUA0(void){
     CNPUA = 0x0000;
     setPinPU(0, 0);
-    TEST_ASSERT_BITS(0xFFFF, 0x0001, CNPUA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0001, CNPUA);
 }
 
 void test_set_CNPUA1(void){
     CNPUA = 0x0000;
     setPinPU(0, 1);
-    TEST_ASSERT_BITS(0xFFFF, 0x0002, CNPUA);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0002, CNPUA);
 }
 
-void test_set_CNPUA15(void){
+void test_set_CNPUA4(void){
     CNPUA = 0x0000;
-    setPinPU(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x8000, CNPUA);
+    setPinPU(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0010, CNPUA);
 }
 
 void test_set_multiple_Pins_PU(void){
     CNPUA = 0x0000;
     setPinPU(0, 0);
     setPinPU(0, 1);
-    setPinPU(0, 15);
-    TEST_ASSERT_BITS(0xFFFF, 0x8003, CNPUA);
+    setPinPU(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0013, CNPUA);
 }
 
 //----Test Setting Pull Down----//
-// void test_set_CNPDA0(void){
-//     CNPDA = 0xFFFF;
-//     setPinPD(0, 0);
-//     TEST_ASSERT_BITS(0xFFFF, 0xFFFE, CNPDA);
-// }
-//
-// void test_set_CNPUA1(void){
-//     CNPUA = 0xFFFF;
-//     setPinPD(0, 1);
-//     TEST_ASSERT_BITS(0xFFFF, 0xFFFD, CNPDA);
-// }
-//
-// void test_set_CNPUA15(void){
-//     CNPUA = 0xFFFF;
-//     setPinPD(0, 15);
-//     TEST_ASSERT_BITS(0xFFFF, 0x7FFF, CNPDA);
-// }
-//
-// void test_set_multiple_Pins_PD(void){
-//     CNPUA = 0x0000;
-//     setPinPD(0, 0);
-//     setPinPD(0, 1);
-//     setPinPD(0, 15);
-//     TEST_ASSERT_BITS(0xFFFF, 0x7FFC, CNPDA);
-// }
+void test_set_CNPDA0(void){
+    CNPDA = 0x0000;
+    setPinPD(0, 0);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0001, CNPDA);
+}
+
+void test_set_CNPDA1(void){
+    CNPDA = 0x0000;
+    setPinPD(0, 1);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0002, CNPDA);
+}
+
+void test_set_CNPDA4(void){
+    CNPDA = 0x0000;
+    setPinPD(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0010, CNPDA);
+}
+
+void test_set_multiple_Pins_PD(void){
+    CNPDA = 0x0000;
+    setPinPD(0, 0);
+    setPinPD(0, 1);
+    setPinPD(0, 4);
+    TEST_ASSERT_BITS(PORTA_PIN_MASK, 0x0013, CNPDA);
+}
 
 
